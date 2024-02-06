315868982ca09ed5ffb61debe55ec770b2d0ed53
PRASHANTPAL283 <93310128+PRASHANTPAL283@users.noreply.github.com>
2024-02-06 22:38:50 +0530

    login commits

--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataservicesService } from '../dataservices.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-component.component.css']
 })
 export class LoginComponentComponent {
+
+  constructor(public service:DataservicesService, public fb:FormBuilder){
+
+  }
+  ngOnInit(){
+
+  }
+
+  loginModel=this.fb.group({
+    username:['',Validators.required],
+    password:['',Validators.required]
+  })
+  getName(){
+    return this.loginModel.get('username');
+  }
+  getPass(){
+    return this.loginModel.get('password');
+  }
+
+  doLogin(){
+    if(!this.loginModel?.valid){
+      this.loginModel.markAllAsTouched();
+    }
+    else{
+      alert("clearred")
+    }
+  }
 
 }
