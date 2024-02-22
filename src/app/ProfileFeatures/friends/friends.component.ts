3a3ebad539245fb43641f3053dc033a704705d3c
PRASHANTPAL283 <93310128+PRASHANTPAL283@users.noreply.github.com>
2024-02-22 12:50:15 +0530

    updte899

--- src/app/ProfileFeatures/friends/friends.component.ts
+++ src/app/ProfileFeatures/friends/friends.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataservicesService } from 'src/app/dataservices.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-friends',
@@ -6,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent {
+
+  constructor(public service:DataservicesService){
+
+  }
+  ngOnInit(){
+    this.currentUser=sessionStorage.getItem("user");
+    this.currentUser=JSON.parse(this.currentUser);
+    this.getUserId();
+   
+   }
+  currentUser:any;
+  allfriends:any=[];
+  allusers:any=[];
+  apiUrl=environment.apiUrl;
+
+  getUserId():any{
+    let id:any=null;
+
+    this.service.getallusers().subscribe({ 
+      next:(val:any)=>{
+        this.allusers=val;
+        console.log(val)
+        val.forEach((val:any)=>{
+          if(val.username===this.currentUser.username){
+            id=val.userId;
+            console.log(id)
+          }
+        })
+      },
+      complete:()=>{
+        this.getallfriendsbyuserid(id); }
+    })
+
+  }
+
+  getallfriendsbyuserid(id:any){
+    this.service.getallfriendsbyuserid(id).subscribe({ 
+      next:(val:any)=>{
+        this.allfriends=val;
+        },
+      complete:()=>{
+
+      }
+    })
+  }
+  
+
 
 }
